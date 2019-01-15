7d7f2ed70b03e312e7abb16d0b935a7485737fee
Eric Torunski <etorunski@gmail.com>
2019-01-15 18:54:07 -0500

    Week 2 examples

--- app/src/main/java/com/example/inclassexamples_w19/MainActivity.java
+++ app/src/main/java/com/example/inclassexamples_w19/MainActivity.java
@@ -4,9 +4,8 @@ package com.example.inclassexamples_w19;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,10 +16,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         //This line says to load activity_main.xml in the res/layout folder:
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_linear);
 
         //After this line, you can start importing your XML Widgets:
-        //Button button1 = (Button) findViewById(R.id.button1);
+        Button button1 = (Button)findViewById(R.id.button1);
 
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numClicks++;
+            }
+        });
+
+
+        //This line is the same as above, but uses Lambda function notation
+        button1.setOnClickListener( v -> numClicks++);
     }
 }
