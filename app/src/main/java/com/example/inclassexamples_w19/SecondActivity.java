ed51fc276a3be92dc935409f5dfbd11ffb411b56
Eric Torunski <etorunski@gmail.com>
2019-01-21 11:13:15 -0500

    week 2 examples

--- app/src/main/java/com/example/inclassexamples_w19/SecondActivity.java
+++ app/src/main/java/com/example/inclassexamples_w19/SecondActivity.java
@@ -2,6 +2,7 @@ package com.example.inclassexamples_w19;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button btn = (Button)findViewById(R.id.previousPageButton);
+
+        btn.setOnClickListener( b -> {
+            setResult(50);
+            //This line goes back to the previous page, and sends 50 as the result.
+            finish();
+        });
+
     }
 }
