6437400e1a51229dda417e66c00d13391f56576f
Eric Torunski <etorunski@gmail.com>
2019-01-29 11:24:53 -0500

    week 3 examples

--- app/src/main/java/com/example/inclassexamples_w19/SharedPreferencesExample.java
+++ app/src/main/java/com/example/inclassexamples_w19/SharedPreferencesExample.java
@@ -22,6 +22,7 @@ public class SharedPreferencesExample extends AppCompatActivity {
         typeField.setText(savedString);
     }
 
+
     @Override
     protected void onPause() {
         super.onPause();
