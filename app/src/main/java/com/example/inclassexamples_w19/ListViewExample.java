77958380c0708d650e262b2613ba66fef7a4f94a
Eric Torunski <etorunski@gmail.com>
2019-01-30 09:51:24 -0500

    finished week4 examples

--- app/src/main/java/com/example/inclassexamples_w19/ListViewExample.java
+++ app/src/main/java/com/example/inclassexamples_w19/ListViewExample.java
@@ -1,13 +1,18 @@
 package com.example.inclassexamples_w19;
 
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -17,24 +22,36 @@ import java.util.List;
 
 public class ListViewExample extends AppCompatActivity {
 
+    int numObjects = 6;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
 
-        ListView theList = (ListView)findViewById(R.id.the_list);
-
-
         //ListAdapter adt = new MyArrayAdapter(new String[] {"A", "B", "C"});
         ListAdapter adt = new MyOwnAdapter();
+
+
+        ListView theList = (ListView)findViewById(R.id.the_list);
+        SwipeRefreshLayout refresher = (SwipeRefreshLayout)findViewById(R.id.refresher) ;
+        refresher.setOnRefreshListener(()-> {
+            numObjects *= 2;
+            ((MyOwnAdapter) adt).notifyDataSetChanged();
+            refresher.setRefreshing( false );
+        });
+
+
         theList.setAdapter(adt);
+
+
         
         //This listens for items being clicked in the list view
-        theList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        theList.setOnItemClickListener(( parent,  view,  position,  id) -> {
+            Log.e("you clicked on :" , "item "+ position);
 
-            }
+        numObjects = 20;
+        ((MyOwnAdapter) adt).notifyDataSetChanged();
         });
     }
 
@@ -42,20 +59,32 @@ public class ListViewExample extends AppCompatActivity {
     protected class MyOwnAdapter extends BaseAdapter
     {
 
-        public int getCount() { return 0; }
+        @Override
+        public int getCount() {
+            return numObjects;
+        }
 
         public Object getItem(int position){
-            return null;
+            return "SHow this in row "+ position;
         }
 
         public View getView(int position, View old, ViewGroup parent)
         {
-            return null;
+            LayoutInflater inflater = getLayoutInflater();
+
+            View newView = inflater.inflate(R.layout.single_row, parent, false );
+
+
+            TextView rowText = (TextView)newView.findViewById(R.id.textOnRow);
+            String stringToShow = getItem(position).toString();
+            rowText.setText( stringToShow );
+            //return the row:
+            return newView;
         }
 
         public long getItemId(int position)
         {
-            return 0;
+            return position;
         }
     }
 
