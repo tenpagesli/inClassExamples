ea13e993b5afdc30425788e973e20abcbdfe072a
Eric Torunski <etorunski@gmail.com>
2019-03-26 09:24:05 -0400

    Merge branch 'master' of https://github.com/etorunski/InClassExamples_W19
    # Conflicts:
    #	app/src/main/java/com/example/inclassexamples_w19/FirstActivity.java

--- app/src/main/java/com/example/inclassexamples_w19/NextActivity.java
+++ app/src/main/java/com/example/inclassexamples_w19/NextActivity.java
@@ -9,5 +9,7 @@ public class NextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+        // branch 2 comment
+
     }
 }
